93ea7ecdb1956168f73947c1276bfb0f1befc64b
myouka <myoukaarata@gmail.com>
2020-07-17 10:48:57 +0700

    soccernews-v2 commit-3

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts("precache-manifest.3a089a3d582bbe01e1df6b30cd1b9175.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-const schedule = "http://127.0.0.1:8887/#todayCompetitions";
-const home = "http://127.0.0.1:8887";
+const schedule = "https://myoukaarata.github.io/#todayCompetitions";
+const home = "https://myoukaarata.github.io";
 
 workbox.precaching.precacheAndRoute([
   { url: "/", revision: "2" },
